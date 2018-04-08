9023e4ba2b029e925ca33adbbe75d3fe79e0b0a5
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2018-04-08 21:51:08 +0800

    created custom marker component

---
+++ src/components/screens/Maps/CustomMarker.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { MapView } from 'expo';
+
+import { GLOBALS } from '../../../Globals';
+
+const CustomMarker = (props) => { 
+  return (
+    <MapView.Marker
+      coordinate={props.marker.coordinate}
+      title={props.marker.title}
+      description={props.marker.description}
+    >
+      <View style={styles.markerView}>
+        <View style={styles.markerRing} />
+      </View>
+    </MapView.Marker>
+  );
+};
+
+const styles = StyleSheet.create({
+  markerView: {
+    backgroundColor: GLOBALS.COLORS.PDARK,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 1
+  },
+  markerRing: {
+    backgroundColor: 'white',
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    margin: 0
+  }
+});
+
+export { CustomMarker };

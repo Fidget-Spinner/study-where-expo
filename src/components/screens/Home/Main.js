dda329e24aeeef7c6eef75ad2f844758f40f45ac
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2018-04-08 21:52:46 +0800

    changed tabs from 4 spaces to 2 spaces

--- src/components/screens/Home/Main.js
+++ src/components/screens/Home/Main.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-  } from 'react-native';
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
+//Main Page Component
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 class Main extends Component {
@@ -18,7 +19,7 @@ class Main extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Hi
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
@@ -33,20 +34,20 @@ class Main extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 0,
+  fontSize: 20,
+  textAlign: 'center',
+  margin: 0,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textAlign: 'center',
+  color: '#333333',
+  marginBottom: 5,
   },
 });
 
--- src/routerConfigs/tabNavConfig.js
+++ src/routerConfigs/tabNavConfig.js
@@ -6,35 +6,35 @@ import GMap from '../components/screens/Maps/GMap';
 import { GLOBALS } from '../Globals';
 
 export const RootStack = TabNavigator(
-    {
-        Home: {
-            screen: Main,
-        },
-        Map: {
-            screen: GMap,
-        },
+  {
+    Home: {
+      screen: Main,
     },
-    {
-        tabBarOptions: {
-            // activeBackgroundColor: GLOBALS.COLORS.P,
-            // inactiveBackgroundColor: '#fc9d9d',
-            activeTintColor: GLOBALS.COLORS.ACTIVE,
-            inactiveTintColor: GLOBALS.COLORS.INACTIVE,
-            indicatorStyle: {
-                backgroundColor: GLOBALS.COLORS.ACTIVE
-            },
-            style: {
-                backgroundColor: GLOBALS.COLORS.PRIMARY,
-            },
-            labelStyle: {
-                fontFamily: GLOBALS.HEADER1
-            },
-            allowFontScaling: true,
-          },
-        initialRouteName: 'Home',
-        lazyLoad: true,
-        tabBarPosition: 'bottom',
-        animationEnabled: true,
-        swipeEnabled: true,
+    Map: {
+      screen: GMap,
     },
+  },
+  {
+    tabBarOptions: {
+      // activeBackgroundColor: GLOBALS.COLORS.P,
+      // inactiveBackgroundColor: '#fc9d9d',
+      activeTintColor: GLOBALS.COLORS.ACTIVE,
+      inactiveTintColor: GLOBALS.COLORS.INACTIVE,
+      indicatorStyle: {
+        backgroundColor: GLOBALS.COLORS.ACTIVE
+      },
+      style: {
+        backgroundColor: GLOBALS.COLORS.PRIMARY,
+      },
+      labelStyle: {
+        fontFamily: GLOBALS.HEADER1
+      },
+      allowFontScaling: true,
+      },
+    initialRouteName: 'Home',
+    lazyLoad: true,
+    tabBarPosition: 'bottom',
+    animationEnabled: true,
+    swipeEnabled: true,
+  },
 );

05f14ff3e64b524a46e68ba4aab37c2459bb96e5
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2018-04-08 23:30:00 +0800

    Added function to recursively render list of custom markers on map

--- src/components/screens/Maps/GMap.js
+++ src/components/screens/Maps/GMap.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    Text
   } from 'react-native';
 import { MapView, Constants } from 'expo';
 
@@ -9,9 +10,45 @@ import { CustomMarker } from './CustomMarker';
 
 //Gmaps MapView wrapper component
 class GMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+        // test code only, in final the
+        // markers will be taken from redux store/ firebase
+        {
+          latlng: {
+            latitude: 1.319103,
+            longitude: 103.891973,
+          },
+          title: 'marker1',
+          description: 'desc1',
+        },
+        {
+          latlng: {
+            latitude: 1.350789,
+            longitude: 103.870933,
+          },
+          title: 'marker1',
+          description: 'desc2',
+        },
+      ]
+      
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
+        {/* DEBUG VIEW: */}
+        {/* <View style={{ width: 150, height: 400, top: 10, left: 10, zIndex: 3, position: 'absolute' }}>
+          {this.state.markers.map((marker, key) => (
+            <Text key={key}>
+              coordinate={[marker.latlng.latitude, marker.latlng.longitude]}{'\n'}
+              title={marker.title}{'\n'}
+              description={marker.description}{'\n'}
+            </Text>
+          ))}
+        </View> */}
         <MapView
           style={styles.map}
           initialRegion={{
@@ -21,14 +58,19 @@ class GMap extends Component {
             longitudeDelta: 0.04,
           }}
         >
-        {/* Test marker */}
-        {/* <MapView.Marker 
-          title="Home"
-          coordinate={{
-            latitude: 1.346706,
-            longitude: 103.842016,
-          }}
-        /> */}
+        {/* this is to take an object with a list of markers and
+        run through them, then display them on screen */}
+        {this.state.markers.map((marker, key) => (
+          <CustomMarker 
+            key={key}
+            marker={{
+              coordinate: marker.latlng,
+              title: marker.title,
+              description: marker.description
+            }}
+          />
+        ))}
+        {/* Home Test Sample Marker: */}
           <CustomMarker 
             marker={{
               coordinate: {

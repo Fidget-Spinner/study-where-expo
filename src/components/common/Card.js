2a05123229617e66c96c4d74e17ee472f3f4fd29
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2018-04-09 22:47:16 +0800

    changed tabs to 2 spaces

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -2,39 +2,39 @@ import React from 'react';
 import { View, } from 'react-native';
 
 const Card = (props) => {
-    return (
-        <View style={styles.container}>
-            {props.children}
-            {// props.children refers to the child component of any props that is passed in to
-            // Card tag/component
-            // in this case it refers to the <Cardsection> component passed to Card in AlbumDetails,
-            //therefore this renders the <Cardsection>...</CardSection> from AlbumDetails
-            }
-        </View>
-        
-    );
+  return (
+    <View style={styles.container}>
+      {props.children}
+      {// props.children refers to the child component of any props that is passed in to
+      // Card tag/component
+      // in this case it refers to the <Cardsection> component passed to Card in AlbumDetails,
+      //therefore this renders the <Cardsection>...</CardSection> from AlbumDetails
+      }
+    </View>
+    
+  );
 };
 
 
 const styles = {
-    container: {
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        // FOR IOS:
-        // shadowColor: '#000',
-        // shadowOffset: { width: 0, height: 2 },
-        // shadowOpacity: 0.1,
-        // shadowRadius: 2,
-        // elevation: 1,
+  container: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    // FOR IOS:
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 2,
+    // elevation: 1,
 
-        //FOR ANDROID:
-        elevation: 2,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 10
-    }
+    //FOR ANDROID:
+    elevation: 2,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
 };
 
 export { Card };
--- src/components/common/TextField.js
+++ src/components/common/TextField.js
@@ -2,55 +2,55 @@ import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
 const TextField = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
-    // pass in props.label, this is in destructured form...
-    const { input, labels, container } = styles;
-    //again more ES6 destructuring
+  // pass in props.label, this is in destructured form...
+  const { input, labels, container } = styles;
+  //again more ES6 destructuring
     return (
-        <View style={container}>
-            <Text style={labels}>{label}</Text>
-            <TextInput
-                secureTextEntry={secureTextEntry}
-                //same meaning as this.props.secureTextEntry
-                //this is pretty interesting, secureTextEntry is set to undefined
-                //by default which is a falsy in JSX, therefore by default it is false
-                // however to initialise as true, instead of secureTextEntry={true},
-                //just type secureTextEntry because that initialises it and is
-                //defined and also has a value, therefore it is now truthy
-                autoCorrect={false}
-                placeholder={placeholder}
-                style={input}
-                value={value}
-                onChangeText={onChangeText}
-                //takes in the props.value and props.onChangeText that is
-                //passed as props to Input component in some component eg LoginForm.js
-                // then set local value = this.props.value and
-                // onChangeText = this.props.onChangeText( which is some function)
-            />
-        </View>
+      <View style={container}>
+        <Text style={labels}>{label}</Text>
+        <TextInput
+          secureTextEntry={secureTextEntry}
+          //same meaning as this.props.secureTextEntry
+          //this is pretty interesting, secureTextEntry is set to undefined
+          //by default which is a falsy in JSX, therefore by default it is false
+          // however to initialise as true, instead of secureTextEntry={true},
+          //just type secureTextEntry because that initialises it and is
+          //defined and also has a value, therefore it is now truthy
+          autoCorrect={false}
+          placeholder={placeholder}
+          style={input}
+          value={value}
+          onChangeText={onChangeText}
+          //takes in the props.value and props.onChangeText that is
+          //passed as props to Input component in some component eg LoginForm.js
+          // then set local value = this.props.value and
+          // onChangeText = this.props.onChangeText( which is some function)
+        />
+      </View>
     );
 };
 
 const styles = {
-    input: {
-        color: '#000',
-        paddingRight: 5,
-        paddingLeft: 5,
-        fontSize: 18,
-        lineHeight: 23,
-        flex: 2
-    },
-    labels: {
-        fontSize: 18,
-        paddingLeft: 20,
-        flex: 1,
-        fontWeight: 'bold'
-    },
-    container: {
-        height: 40,
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center'
-    }
+  input: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2
+  },
+  labels: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1,
+    fontWeight: 'bold'
+  },
+  container: {
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 };
 
 export { TextField };

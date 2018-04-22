7b972bfcde443410f8cc56e14401c2b2fad6e9d5
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2018-04-22 15:23:15 +0800

    comments for popup

--- src/components/common/PopUp.js
+++ src/components/common/PopUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Modal, TouchableHighlight, Text } from 'react-native';
 
+//******** WIP *******
 class PopUp extends Component {
   constructor(props) {
     super(props);
